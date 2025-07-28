396bee4d38b75c1fb117529271600152fa39fae4
atharva1010 <onlymahi60@gmail.com>
2025-07-28 11:40:23 +0530

    Update Update.js

--- models/Update.js
+++ models/Update.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
 const updateSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  message: { type: String, required: true },
-  image: { type: String },
-  createdAt: { type: Date, default: Date.now }
+  title: String,
+  message: String,
+  image: String, // Cloudinary image URL
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("Update", updateSchema);
